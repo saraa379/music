3cd90bc42db398da0f732a404ebe9f0c49ea5468
saraa379 <saraa379@yahoo.com>
2020-09-30 16:13:07 +0200

    updated

--- script/scripts.js
+++ script/scripts.js
@@ -1,3 +1,26 @@
+
+//Global variables - db for songs
+const songs = [
+  {
+    title: 'We Know The Way',
+    artist: 'Moana',
+    image: 'img/moana.jpg',
+    url: 'https://www.youtube.com/watch?v=ubZrAmRxy_M&list=WL&index=29'
+  },
+	{
+		title: 'Black Thunder',
+		artist: 'The Hu',
+		image: 'img/hu.jpg',
+    url: 'https://www.youtube.com/watch?v=epAhB39OXSU&list=WL&index=41'
+	},
+	{
+    title: 'Requiem',
+		artist: 'Altan Urag',
+		image: 'img/altan-urag.jpg',
+    url: 'https://www.youtube.com/watch?v=UabFYUqITg8&list=WL&index=45'
+	}
+];
+
 window.addEventListener('load', function(event) {
 
 
