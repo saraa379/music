e004090801dc1c5ee9594b17dd0536a981923436
saraa379 <saraa379@yahoo.com>
2020-10-01 12:40:53 +0200

    added songs in the db

--- script/scripts.js
+++ script/scripts.js
@@ -18,6 +18,78 @@ const songs = [
 		artist: 'Altan Urag',
 		image: 'img/altan-urag.jpg',
     url: 'https://www.youtube.com/watch?v=UabFYUqITg8&list=WL&index=45'
+	},
+  {
+    title: 'True Sisters',
+		artist: 'Sofia the First',
+		image: 'img/SofiaCinderela.jpg',
+    url: 'https://www.youtube.com/watch?v=abSYQXBJvGc'
+	},
+  {
+    title: 'The Simple Life',
+		artist: 'Sofia the First',
+		image: 'img/The-Simple-Life.png',
+    url: 'https://www.youtube.com/watch?v=-kv10oU0Bro&list=WL&index=22'
+	},
+  {
+    title: 'Let It Go',
+		artist: 'Frozen',
+		image: 'img/frozen.jpg',
+    url: 'https://www.youtube.com/watch?v=BKCsFB5bPz8&list=WL&index=27'
+	},
+  {
+    title: 'Brother Louie',
+		artist: 'Modern Talking',
+		image: 'img/modern-talking.jpg',
+    url: 'https://www.youtube.com/watch?v=Lp2qcCrdBLA&list=WL&index=9'
+	},
+  {
+    title: 'Dancing Queen',
+		artist: 'ABBA',
+		image: 'img/abba.jpg',
+    url: 'https://www.youtube.com/watch?v=xFrGuyw1V8s'
+	},
+  {
+    title: 'Suteki Da Ne',
+		artist: 'Final Fantasy X',
+		image: 'img/final-fantasy.jpg',
+    url: 'https://www.youtube.com/watch?v=xlwSPMwAqbM'
+	},
+  {
+    title: 'Last Christmas',
+		artist: 'Wham!',
+		image: 'img/wham.jpg',
+    url: 'https://www.youtube.com/watch?v=E8gmARGvPlI'
+	},
+  {
+    title: 'Nas Ne Dogonyat',
+		artist: 't.A.T.u.',
+		image: 'img/tatu.jpg',
+    url: 'https://www.youtube.com/watch?v=PRMgavw2bZ4'
+	},
+  {
+    title: 'Babe',
+		artist: 'Take That',
+		image: 'img/take-that.jpg',
+    url: 'https://www.youtube.com/watch?v=MviFdVHltEM'
+	},
+  {
+    title: 'Time To Say Goodbye',
+		artist: 'Sarah Brightman',
+		image: 'img/Sarah_Brightman.jpg',
+    url: 'https://www.youtube.com/watch?v=mH4osTwddUg'
+	},
+  {
+    title: 'Viva Forever',
+		artist: 'Spice Girls',
+		image: 'img/Spice-Girls.jpg',
+    url: 'https://www.youtube.com/watch?v=3wkuqRFXNvI'
+	},
+  {
+    title: 'Living In A Fantasy',
+		artist: 'BWO',
+		image: 'img/bwo.png',
+    url: 'https://www.youtube.com/watch?v=RQ1Mi9G78hI'
 	}
 ];
 
